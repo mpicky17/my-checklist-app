bc7f2d5b6c9b1ebaef290578116b9b7881bc7f88
mpicky17 <mpicky17@hotmail.com>
2026-03-28 16:45:45 -0400

    Bust service worker cache to pick up Firebase update

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Cache-first strategy: serve from cache, fall back to network.
 // To force a cache refresh after updating the app, bump CACHE_NAME to 'my-checklist-v2'.
 
-const CACHE_NAME = 'my-checklist-v1';
+const CACHE_NAME = 'my-checklist-v2';
 
 const ASSETS_TO_CACHE = [
   './',
